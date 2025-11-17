a5370b7f181d895dbe0d9a7c08140d9569624f9f
physicscoreai <physicsaiconcept@gmail.com>
2025-11-17 01:06:56 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error
+
 import streamlit as st
 import numpy as np
 
