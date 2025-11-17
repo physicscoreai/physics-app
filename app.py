54f71088894efb72b9febea31a68d056b3b3b292
physicscoreai <physicsaiconcept@gmail.com>
2025-11-17 00:03:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
 st.title("Physics Simulator")
-st.write("My physics app is working!")
+st.write("Welcome to my physics app!")
+
+velocity = st.slider("Choose Velocity", 1, 100, 50)
+angle = st.slider("Choose Angle", 1, 90, 45)
+
+st.write("You selected velocity:", velocity)
+st.write("You selected angle:", angle)
